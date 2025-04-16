bc7974dd21052b8b4d1c1141d30068ea4c6bce15
ilcrosby001 <ilcrosby001@converse.edu>
2025-04-16 11:31:05 -0400

    Three unique pages display ID

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Final Project' });
+  req.app.locals.formdata = {};
+  renderIndex(req, res, next, 'Final Project')
+  // res.render('index', { title: 'Final Project' });
 });
 
 /* POST home page. */
@@ -11,5 +13,17 @@ router.post('/', function(req, res, next) {
   req.app.locals.formdata = req.body;
   renderIndex(req, res, next, ' ');
 });
+
+
+function renderIndex(req, res, next, pagetitle) {
+  if (!req.app.locals.formdata.role) {
+    res.render('index', {title: pagetitle});
+  }
+  else {
+    res.render(req.app.locals.formdata.role, {role: req.app.locals.formdata.role});
+  }
+  
+}
+
 
 module.exports = router;
