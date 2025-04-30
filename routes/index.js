972d6bbdafcb23d04b0022227ff4eeb604e5608b
ilcrosby001 <ilcrosby001@converse.edu>
2025-04-30 13:40:47 -0400

    registrar displays correct courses

--- routes/index.js
+++ routes/index.js
@@ -34,6 +34,9 @@ function renderIndex(req, res, next, pagetitle) {
   else if (req.app.locals.formdata.role === 'student') {
     renderStudent(req, res, next, pagetitle);
   }
+  else if (req.app.locals.formdata.role === 'registrar') {
+    renderRegistrar(req, res, next, pagetitle);
+  }
   else {
     res.render(req.app.locals.formdata.role, {role: req.app.locals.formdata.role});
   }
@@ -131,5 +134,56 @@ function renderStudent(req, res, next, pagetitle){
   });  
 }
 
+function renderRegistrar(req, res, next, pagetitle){
+  let query = 'select OfferNo, CourseNo, Faculty.FacSSN, FacLastName, OffLocation, OffTime, OffDays';
+  query += ' from Offering left join Faculty on Faculty.FacSSN = Offering.FacSSN';
+  query += ` where OffTerm = 'WINTER' and OffYear = 2025`;
+  console.log('Query: ' + query);
+  req.app.locals.query = query;
+  console.log('r.a.l.q:' + req.app.locals.query);
+  req.app.locals.db.all(query, [], 
+      (err, rows) => {
+          if (err) {
+              throw err;
+          }
+          req.app.locals.courses = rows;
+
+          console.log('Canceling Offering: "' + req.app.locals.formdata.cancelOffer + '"')
+          if (req.app.locals.formdata.cancelOffer) {
+              let query2 = 'select OfferNo, CourseNo, Faculty.FacSSN, FacLastName, OffLocation, OffTime, OffDays';
+              query2 += ' from Offering left join Faculty on Faculty.FacSSN = Offering.FacSSN';
+              query2 += ` where OfferNo = '${req.app.locals.formdata.cancelOffer}';`
+              console.log('Query2 ' + query2);
+              req.app.locals.query2 = query2;
+              req.app.locals.db.all(query2, [],
+                  (err, students) => {
+                      if (err) {
+                          throw err;
+                      }
+                      req.app.locals.offers = offers;
+                      actuallyRenderRegistrar(req, res, next, 
+                          capitalizeString(req.app.locals.formdata.role), req.app.locals.courses,
+                          req.app.locals.formdata.cancelOffer, offers);
+                    }
+              ) 
+          }
+          else {
+              /* Has to happen inside handler, to wait for rows */
+              actuallyRenderRegistrar(req, res, next, 
+                  capitalizeString(req.app.locals.formdata.role), req.app.locals.courses);
+          }
+          // res.render(req.app.locals.formdata.role, 
+          //   {role: capitalizeString(req.app.locals.formdata.role),
+          //   courses: req.app.locals.courses,
+          //   query: req.app.locals.query});
+  });  
+}
+
+function actuallyRenderRegistrar(req, res, next, role, courses, cancelOffer=undefined, offers=undefined) {
+  res.render('registrar', {role: role,
+                        courses: courses,
+                        cancelOffer: cancelOffer,
+                        offers: offers});
+}
 
 module.exports = router;
